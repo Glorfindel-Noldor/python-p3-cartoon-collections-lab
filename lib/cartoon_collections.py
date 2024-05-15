c7ab81c8e1ac88669121a330dfc0287001189496
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-15 17:05:33 -0500

    running through lists

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(arg):
+    for int, i in enumerate(arg, start=1):
+        print(f"{int}. {i}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(arg):
+    result = []
+    for word in arg:
+        result.append(word.capitalize() + '!')
+    return result
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(arg):
+    for i in arg:
+        if len(i) > 4:
+            return True
+    return False
+
+
+def find_the_cheese(arg):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in arg:
+        if item in cheeses:
+            return item
+    return None
